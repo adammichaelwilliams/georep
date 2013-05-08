b5b8b109eea43ba69c260c057b15e26ebd04af19
Adam Williams <kainolophobia@gmail.com>
2013-05-08 17:44:29 -0400

    location to reps working

--- server.py
+++ server.py
@@ -11,25 +11,30 @@ import tornado.web
 import json
 import urllib2
 
+congress_api_key = '50f025f2cbae45078f52d0be51f69027'
+
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
     self.redirect("/static/map.html")
+
+class LocationHandler(tornado.web.RequestHandler):
+  def post(self):
+    lat = self.get_argument('lat')
+    lng = self.get_argument('lng')
+    data = json.load(urllib2.urlopen('http://congress.api.sunlightfoundation.com/legislators/locate?latitude='+lat+'&longitude='+lng+'&apikey='+congress_api_key))
+    print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+    pretty_data = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+    self.render('location.html', result_data=data, pretty_data=pretty_data)
 
 settings = {
   'static_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), ""),
 }
 print settings
 
-latitude = '42.280077'
-longitude = '-83.74188199999998'
-congress_api_key = '50f025f2cbae45078f52d0be51f69027'
 
-data = json.load(urllib2.urlopen('http://congress.api.sunlightfoundation.com/legislators/locate?latitude='+latitude+'&longitude='+longitude+'&apikey='+congress_api_key))
-print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
-
-application = tornado.web.Application([
-  (r"/", MainHandler),
-  ],
+application = tornado.web.Application(
+  handlers = [(r"/", MainHandler),(r"/location", LocationHandler)],
+  template_path=os.path.join(os.path.dirname(__file__), "templates"),
   **settings)
 
 if __name__ == "__main__":
