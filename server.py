83d6269253adfbb3a0c9b90507d4f07ef596dfa6
Adam Williams <kainolophobia@gmail.com>
2013-05-08 13:23:04 -0400

    map options, nothing much else

--- server.py
+++ server.py
@@ -8,6 +8,9 @@ import os
 import tornado.ioloop
 import tornado.web
 
+import json
+import urllib2
+
 class MainHandler(tornado.web.RequestHandler):
   def get(self):
     self.redirect("/static/map.html")
@@ -16,6 +19,13 @@ settings = {
   'static_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), ""),
 }
 print settings
+
+latitude = '42.280077'
+longitude = '-83.74188199999998'
+congress_api_key = '50f025f2cbae45078f52d0be51f69027'
+
+data = json.load(urllib2.urlopen('http://congress.api.sunlightfoundation.com/legislators/locate?latitude='+latitude+'&longitude='+longitude+'&apikey='+congress_api_key))
+print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
 
 application = tornado.web.Application([
   (r"/", MainHandler),
